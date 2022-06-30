1608e2b2d2b8f46be774de10b0acd1f44cb61fda
swseighman <scottseighman@gmail.com>
2022-06-29 22:15:42 -0400

    Source and README updates

--- src/main/java/com/example/SpringBootFunction.java
+++ src/main/java/com/example/SpringBootFunction.java
@@ -6,14 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWebApplication extends SpringBootServletInitializer{
+public class SpringBootFunction extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWebApplication.class, args);
+		SpringApplication.run(SpringBootFunction.class, args);
 	}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(SpringBootWebApplication.class);
+		return builder.sources(SpringBootFunction.class);
 	}
 }
